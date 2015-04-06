e01280287a1eb168cc6a807fad3889150e67254e
justinpille <justin.pille@gmail.com>
2015-04-06 00:05:23 -0500

    Generating a 2d array of matched pattern

--- markour.js
+++ markour.js
@@ -1,24 +1,13 @@
-function checkChar(char) {
-  result = '';
-  switch (char) {
-    case '*':
-      result = '<h1>';
-      break;
-    case '%':
-      result = '</h1>'
-      break;
-    default:
-      result = char;
-  }
-  return result;
-}
-
 var markour = function(src) {
-  var result = '';
-  var array = src.split('');
-  for (var i = 0; i < array.length; i++) {
-    var transformed = checkChar(array[i]);
-    result += transformed;
+
+  var myRe = /::nr/g;
+  var str = src;
+  var each = [];
+  var all = [];
+
+  while ((each = myRe.exec(str)) !== null){
+    all.push([each.index,myRe.lastIndex]);
   }
-  return result;
-}
+
+
+};
