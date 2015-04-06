7ac8d7872addb0034489422032ed350a59427ea7
justinpille <justin.pille@gmail.com>
2015-04-06 18:17:30 -0500

    refactored entire project

--- applyRules.js
+++ applyRules.js
@@ -1,10 +1,35 @@
-function applyRules(prepped, data) {
-  return prepped;
-
+function applyIt(text, data) {
+/*
   // If the line starts with '<!--', do nothing
   // If the line starts with a user-defined pattern (before the delimiter), and if that pattern specifies '\n' after the delimiter, transform the whole line
   // If the line starts with a user-defined pattern (before the delimiter), and that pattern doesn't specify '\n' after the delimiter, transform only what's inbetween the two sub-patterns
+  // If the line has a user-defined pattern embedded...
 
+  // Check for special symbols at the beginning of the line
+  // Return all data indexes that match
+  function beginsWithSymbol(line) {
+    var dataMatch = 0;
+    for (var i = 0; i < data.length; i++) {
+      var subPatA = data[i].pattern.split(0, '::');
+      var regex = /subPatA/g;
+      var result = regex.exec(line);
+      if (result !== null && result.index === 0) { //If it matches at the beginning of the line
+        dataMatch = i;
+      }
+    }
+    return dataMatch;
+  }
+
+  for (var i = 0; i < text.length; i++) {
+    var dataMatch;
+    if (text[i].slice(0, 4) !== '<!--') { // Don't apply rules to comments
+      dataMatch = beginsWithSymbol(text[i]);
+    }
+    //console.log(data[dataMatch].result);
+  }
+*/
+console.log(text);
+return text;
 
 
 
--- markour.js
+++ markour.js
@@ -1,94 +1,79 @@
-function mkLines(src) {
-  return src.split(/\r?\n|\r/);
+function mkLines(str) {
+  return str.split(/\r?\n|\r/);
 }
-
-function readRules(src) {
-
-  function findMK(lines) {
-    var mkLocations = [];
-    var mkGlyph = /:mk:/g;
-    for (var i = 0; i < lines.length; i++) {
-      if (mkGlyph.exec(lines[i]) != null) {
-        mkLocations.push(i);
-      }
+function findMK(lines) {
+  var mkLocations = [];
+  var mkGlyph = /:mk:/g;
+  for (var i = 0; i < lines.length; i++) {
+    if (mkGlyph.exec(lines[i]) !== null) {
+      mkLocations.push(i);
     }
-    return mkLocations;
   }
-
-  function getVals(mkLocations, lines, offset) {
-    var patternVals = [];
-    for (var i = 0; i < mkLocations.length; i++) {
-      patternVals[i] = lines[mkLocations[i] + offset];
-    }
-    return patternVals;
-  }
-
-
-
-  var lines = mkLines(src); // Array where each index is a line number
-  var mkLocations = findMK(lines); // Array of line numbers where mk declaration is found
-  var patternVals = getVals(mkLocations, lines, 1); // Values of the next line after mk declaration
-  var resultsVals = getVals(mkLocations, lines, 2); // Values of the 2nd line after mk declaration
-
-
-  function buildData(mkLocations, patternVals, resultsVals) {
-    var rules = [];
-    for (var i = 0; i < mkLocations.length; i++) {
-      rules.push({
-        startingAt: mkLocations[i] + 3,
-        pattern: patternVals[i],
-        result: resultsVals[i]
-      })
-    }
-    return (rules);
-  }
-
-
-  var rules = buildData(mkLocations, patternVals, resultsVals);
-  return rules;
+  return mkLocations;
 }
-
-function prepareSrc(src, data) {
-
-  // Add line breaks
-  function breakLines(lines) {
-    var brokenLines = [];
-    for (var i = 0; i < lines.length; i++) {
-      brokenLines[i] = lines[i] + '<br />'
-    }
-    return brokenLines;
+function getVals(mkLocations, lines, offset) {
+  var patternVals = [];
+  for (var i = 0; i < mkLocations.length; i++) {
+    patternVals[i] = lines[mkLocations[i] + offset];
   }
-
-  // Comment out rule blocks
-  function commOut(brokenLines) {
-    //generate list of line numbers to be commented out
-    var commNums = [];
-    for (var i = 0; i < data.length; i++) {
-      commNums.push(data[i].startingAt -3);
-      commNums.push(data[i].startingAt -2);
-      commNums.push(data[i].startingAt -1);
-    }
-    //console.log(commNums);
-    var commLines = [];
-    for (var i = 0; i < brokenLines.length; i++) {
-      if ($.inArray(i, commNums) === -1) { // If the index is not in commNums
-        commLines[i] = brokenLines[i];
-      } else {
-        commLines[i] = '<!--' + brokenLines[i] + '-->';
-      }
-    }
-    return commLines;
+  return patternVals;
+}
+function buildData(mkLocations, patternVals, resultsVals) {
+  var rules = [];
+  for (var i = 0; i < mkLocations.length; i++) {
+    rules.push({
+      startingAt: mkLocations[i] + 3,
+      pattern: patternVals[i],
+      result: resultsVals[i]
+    });
   }
-
-
-
-
-  // Don't apply rules to comments
-
-
-
-  var lines = mkLines(src); // Array where each index is a line number
-  var brokenLines = breakLines(lines);
-  var commLines = commOut(brokenLines);
+  return (rules);
+}
+function breakLines(lines) {
+  var brokenLines = [];
+  for (var i = 0; i < lines.length; i++) {
+    brokenLines[i] = lines[i] + '<br />';
+  }
+  return brokenLines;
+}
+function commOut(brokenLines, data) {
+  var commNums = []; //generate list of line numbers to be commented out
+  for (var i = 0; i < data.length; i++) {
+    commNums.push(data[i].startingAt - 3);
+    commNums.push(data[i].startingAt - 2);
+    commNums.push(data[i].startingAt - 1);
+  }
+  var commLines = [];
+  for (var j = 0; j < brokenLines.length; j++) {
+    if ($.inArray(j, commNums) === -1) { // If the index is not in commNums
+      commLines[j] = brokenLines[j];
+    } else {
+      commLines[j] = '<!--' + brokenLines[j] + '-->';
+    }
+  }
   return commLines;
 }
+
+
+
+
+function run(src) {
+  var lines = mkLines(src); // Array where each index is a line number
+  function prepData(lines){
+    var mkLocations = findMK(lines); // Array of line numbers where mk declaration is found
+    var patternVals = getVals(mkLocations, lines, 1); // Values of the next line after mk declaration
+    var resultsVals = getVals(mkLocations, lines, 2); // Values of the 2nd line after mk declaration
+    return buildData(mkLocations, patternVals, resultsVals);
+  }
+  function prepText(lines, data){
+    var brokenLines = breakLines(lines);
+    return commOut(brokenLines, data);
+  }
+  var data = prepData(lines);
+  var text = prepText(lines, data);
+  return [data, text];
+}
+
+
+
+//
