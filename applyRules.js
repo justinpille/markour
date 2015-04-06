d038618b0843812c3ccae029bf97292f1cb2b82c
justinpille <justin.pille@gmail.com>
2015-04-06 13:48:00 -0500

    added comments to applyRules.js

--- applyRules.js
+++ applyRules.js
@@ -1,3 +1,11 @@
 function applyRules(prepped, data) {
   return prepped;
+
+  // If the line starts with '<!--', do nothing
+  // If the line starts with a user-defined pattern (before the delimiter), and if that pattern specifies '\n' after the delimiter, transform the whole line
+  // If the line starts with a user-defined pattern (before the delimiter), and that pattern doesn't specify '\n' after the delimiter, transform only what's inbetween the two sub-patterns
+
+
+
+
 }
