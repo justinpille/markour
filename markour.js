a5e60b879bc435a7ae4c3bda33f5e193d8c4dff0
justinpille <justin.pille@gmail.com>
2015-04-06 01:37:15 -0500

    updated markour.js

--- markour.js
+++ markour.js
@@ -1,13 +1,44 @@
 var markour = function(src) {
 
-  var myRe = /::nr/g;
-  var str = src;
-  var each = [];
-  var all = [];
+  var output = '';
 
-  while ((each = myRe.exec(str)) !== null){
-    all.push([each.index,myRe.lastIndex]);
+  function log(remaining) {
+    console.log(output);
+    console.log(remaining);
   }
+
+  // Function
+  // Grab everything between the next two line breaks == user-defined pattern
+  // Grab everything between the two line breaks after that == user-defined result
+  // Pass them into a rule storing function
+  // Pass along what's left of the string to be parsed back into the match function
+
+
+  // Function
+  // Takes the output of the match function and checks it against stored rules
+
+
+
+  // Function: match
+  // Recieves a copy of the string to be parsed
+  // Finds a match, which is specified as an argument
+  // Adds everything before the match to the overall result, then removes it from the string to be parsed (copy)
+  // Deletes the match from the string to be parsed (copy)
+  // Executes a function, which is specified as an argument, passing along what's left of the string to be parsed (copy)
+
+  function match(regex, inStr, thenWhat) {
+    var result = regex.exec(inStr);
+    var start = result.index;
+    var end = regex.lastIndex;
+    output += inStr.slice(0, start);
+    var outStr = inStr.slice(end);
+    thenWhat(outStr);
+  }
+
+  match(/::mk/g, src, log);
+
+  //var newRule = /::nr/g;
+  //var lineBr = /\r?\n|\r/g;
 
 
 };
